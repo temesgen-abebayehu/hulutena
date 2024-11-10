1b890964d44267d533900d7d0c193596c51a874f
temesgen-abebayehu <temesgensoftware21@gmail.com>
2024-11-10 16:04:09 +0300

    add verifyProfile controller and route for user profile verification

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/User.model.js";
+import Verification from "../models/Verification.model.js";
 
 export const getUser = async (req, res) => {
   try {
@@ -34,5 +35,50 @@ export const updateUser = async (req, res) => {
   } else {
     console.log(req.user._id.toString(), req.params.id);
     res.status(403).json({ message: "You can only update your account" });
+  }
+};
+
+export const verifyProfile = async (req, res) => {
+  if (req.user._id.toString() === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const {idCard} = req.body;
+      if (!idCard) {
+        return res.status(400).json({ message: "Please provide id card image" });
+      }      
+      if(user.isVerified){
+        return res.status(400).json({ message: "Profile already verified" });
+      }
+
+      const {degreeCertificate} = req.body;
+      if(user.role === "doctor"){        
+        if (!degreeCertificate) {
+          return res.status(400).json({ message: "Please provide degree certificate image" });
+        }
+      }
+
+      if(!req.body.livenessVerified){
+        return res.status(400).json({ message: "Please provide liveness verification" });
+      }
+
+      const verification = new Verification({
+        userId: req.params.id,
+        idCard: idCard,
+        degreeCertificate: degreeCertificate,
+        livenessVerified: req.body.livenessVerified,
+      });
+
+      await verification.save();
+
+      user.isVerified = true;
+      await user.save();
+      res.status(200).json({ message: "Profile verified successfully." });
+    }
+    catch (error) {
+      console.log(`Error in verifyProfile: ${error.message}`);
+      res.status(404).json({ message: error.message });
+    }
+  } else {
+    res.status(403).json({ message: "You can only verify your account" });
   }
 };
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { protectRoute } from '../middlewares/protectRoute.js';
-import { getUser, updateUser } from '../controllers/user.controller.js';
+import { getUser, updateUser, verifyProfile } from '../controllers/user.controller.js';
 
 
 const router = express.Router();
 
 router.get('/:id',protectRoute, getUser);
 router.put('/:id',protectRoute, updateUser);
+router.post('/verify/:id',protectRoute, verifyProfile);
 
 
 export default router;
