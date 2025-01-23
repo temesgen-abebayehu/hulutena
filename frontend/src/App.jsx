b16d065a6d10bc9dc542e6edec62ff89b60edaa7
temesgen-abebayehu <temesgensoftware21@gmail.com>
2025-01-23 23:00:02 +0300

    replace Appointment, Doctors, and Profile pages with Services, Community, and Resources pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,24 @@ import {  Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import LoginPage from "./pages/LoginPage";
-import AppointmentPage from "./pages/AppointmentPage";
 import RegisterPage from "./pages/RegisterPage";
-import DoctorsPage from "./pages/DoctorsPage";
-import ProfilePage from "./pages/ProfilePage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Resourses from "./pages/Resourses";
+import Community from "./pages/Community";
+import Services from "./pages/Services";
 
 function App() {
   return (
     <div>
       <Header />
     <Routes>
-      <Route path="/appointment" element={<AppointmentPage />} />
+      <Route path="/services" element={<Services />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/doctors" element={<DoctorsPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/resources" element={<Resourses />} />
+      <Route path="/community" element={<Community />} />
       <Route path="/" element={<HomePage />} />
     </Routes>
     <Footer />
